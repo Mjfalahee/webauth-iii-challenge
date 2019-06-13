00ef8c07811fa62f6a24a49b7e4cff3627caa909
mjfalahee <mjfalahee@gmail.com>
2019-06-13 14:56:25 -0700

    Added a quick logout button to clear localstorage

--- auth-react-app/src/App.js
+++ auth-react-app/src/App.js
@@ -5,12 +5,20 @@ import './App.css';
 import Login from './auth/Login.js';
 
 class App extends React.Component {
+
+  logoutHandler = e => {
+    e.preventDefault();
+    localStorage.removeItem('token');
+    window.location.reload();
+  }
+
   render() {
     return (
       <div className="App">
         <h3> Auth Challenge. </h3>
         <nav>
           <NavLink to="/login"> Login </NavLink>
+          <button onClick={this.logoutHandler}> Logout </button>
         </nav>
         <Route path="/login" component={Login}></Route>
       </div>
